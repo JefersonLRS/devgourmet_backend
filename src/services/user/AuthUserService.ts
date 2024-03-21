62816d4379088bcab62735a6625961f07d11f478
JefersonLRS <contato.jefersonlrs@gmail.com>
2024-03-21 16:30:44 -0300

    Fixed: Login Authentication and JWT already done

---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,55 @@
+import prismaClient from "../../prisma"
+import { compare } from "bcryptjs"
+import { sign } from 'jsonwebtoken'
+
+interface AuthRequest {
+    email: string;
+    password: string;
+}
+
+class AuthUserService {
+    async execute({ email, password }: AuthRequest) {
+        
+        if (!email || !password) {
+            throw new Error("All fields are required");
+        }
+
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if (!user) {
+            throw new Error("Email/Password incorrect");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email/Password incorrect");
+        }
+
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: "1d"
+            }
+        )
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
+
+    }
+}
+
+export { AuthUserService }
