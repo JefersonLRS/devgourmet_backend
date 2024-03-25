935cc19406d6d07856fdaca44c20803656712ab7
JefersonLRS <contato.jefersonlrs@gmail.com>
2024-03-25 11:00:37 -0300

    feat: Middleware to verify JWT and authenticate user

---
+++ src/middlewares/isAuthenticated.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+
+interface Payload {
+    sub: string
+}
+
+export function isAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+
+    const authToken = req.headers.authorization
+    
+    if (!authToken) { res.status(401).json({ error: 'Unauthorized' }) }
+
+    const [, token] = authToken.split(" ");
+    
+    try {
+        const { sub } = verify(
+            token,
+            process.env.JWT_SECRET
+        ) as Payload
+
+        console.log(sub);
+        return next();
+        
+    } catch (error) {
+        return res.status(401).json({ error: 'Unauthorized' })
+    }
+    
+}
